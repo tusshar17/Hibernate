9985ddade24aace3e731870173f1e8f475e2db50
tusshar17 <tr.jakhara019@gmail.com>
2023-11-05 01:08:10 +0530

    HQL Complex Queries

--- src/main/java/org/hql/HQLExample.java
+++ src/main/java/org/hql/HQLExample.java
@@ -4,9 +4,12 @@ import jakarta.persistence.Query;
 import org.example.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class HQLExample {
 
@@ -17,19 +20,51 @@ public class HQLExample {
 
         // HQL
         // Syntax
-        String query = "from Student where city = :x ";
 
+
+        String query = "from Student where city = :x ";
         Query q = s.createQuery(query);
         q.setParameter("x", "Jaipur");
 
         // single - unique
         // multiple - list
-
         List<Student> studentList = q.getResultList();
-
         System.out.println(studentList);
+        System.out.println("-----------------");
 
 
+
+        Transaction tx = s.beginTransaction();
+
+        // Delete Query
+        /*
+        Query q2 = s.createQuery("delete from Student where city = :c");
+        q2.setParameter("c", "Kanpur");
+        int numOfRecordsDeleted = q2.executeUpdate();  // return number (how many records deleted)
+        System.out.println("Deleted " + numOfRecordsDeleted + " record(s)");
+         */
+
+
+
+        // Update Query
+        Query q3 = s.createQuery("update Student set city= :c where name = :n");
+        q3.setParameter("c", "Raipur");
+        q3.setParameter("n", "Rohan");
+        int numOfRecordsUpdated = q3.executeUpdate();
+        System.out.println(numOfRecordsUpdated + " record(s) updated.");
+
+
+
+        // Join Query
+        Query q4 = s.createQuery("select a.authorName, b.bookTitle from Author as a INNER JOIN a.books as b");
+        List<Object []> listOfBooks = q4.getResultList();
+
+        for (Object [] arr:listOfBooks) {
+            System.out.println(Arrays.toString(arr));
+        }
+
+
+        tx.commit();
         s.close();
         factory.close();
 
